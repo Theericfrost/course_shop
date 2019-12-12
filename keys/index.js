497a993560fb01060add330fe8e863d2f904a0b9
Theericfrost <38252338+Theericfrost@users.noreply.github.com>
2019-12-12 14:05:31 +0300

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    MONGODB_URI : 'mongodb+srv://theericfrost:35USH5RY4HIHfEjM@cluster0-eetlk.mongodb.net/shop',
+    MONGODB_URI : 'mongodb+srv://******:****@cluster0-eetlk.mongodb.net/shop',
     SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
+    SENDGRID_API_KEY: '****',
     EMAIL_FROM: 'nodejs@email.ru',
     BASE_URL: 'https://protected-citadel-51964.herokuapp.com'
 }
