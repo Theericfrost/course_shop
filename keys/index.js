08bee4e39b9275fe3af113c75d5a17cac4681793
Theericfrost <38252338+Theericfrost@users.noreply.github.com>
2019-12-12 14:06:08 +0300

    Without keys

--- keys/index.js
+++ keys/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI : 'mongodb+srv://******:****@cluster0-eetlk.mongodb.net/shop',
+    MONGODB_URI : 'mongodb+srv://****:****@cluster0-eetlk.mongodb.net/shop',
     SESSION_SECRET: 'some secret value',
     SENDGRID_API_KEY: '****',
     EMAIL_FROM: 'nodejs@email.ru',
